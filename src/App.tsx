2368f2f62fe50d3a43978d72e1973a368a5c4163
Praneeth860 <mpraneeth474@gmail.com>
2024-11-03 20:35:23 +0530

    added folder structure , add navbar,pages and routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
-import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Navbar from "./components/NavBar";
+import Home from "./pages/Home";
+import Store from "./pages/Store";
+import About from "./pages/About";
 
-function App() {
-  return <div>you can do this site</div>;
-}
+const App = () => {
+  return (
+    <>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/store" element={<Store />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+    </>
+  );
+};
 
 export default App;
---
+++ src/pages/Store.tsx
@@ -0,0 +1,10 @@
+const Store = () => {
+  return (
+    <div>
+      <h1>Welcome to the Store Page</h1>
+      <p>This is where you can find our products.</p>
+    </div>
+  );
+};
+
+export default Store;
