1187bb5301b6df0ffd93eeebad42ef600885685d
Praneeth860 <mpraneeth474@gmail.com>
2024-11-18 21:50:00 +0530

    added navbar and its css not yet fully finished navbar,just removed default styles

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,20 +1,20 @@
-import React from "react";
 import { Link } from "react-router-dom";
-
+import "../styles/components/navbar.css";
 const Navbar = () => {
   return (
-    <nav>
-      <ul>
-        <li>
+    <nav className="navbar">
+      <ul className="navbar-list">
+        <li className="navbar-item">
           <Link to="/">Home</Link>
         </li>
-        <li>
+        <li className="navbar-item">
           <Link to="/store">Store</Link>
         </li>
-        <li>
+        <li className="navbar-item">
           <Link to="/about">About</Link>
         </li>
       </ul>
+      <button>hi</button>
     </nav>
   );
 };
