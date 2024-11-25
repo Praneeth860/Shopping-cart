790be87bf7df11b10448172d9f7fbd5a08e8d97a
Praneeth860 <mpraneeth474@gmail.com>
2024-11-25 22:16:57 +0530

    added add to car button and quantity controls , remove button

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -11,6 +11,13 @@ const Card = ({ product }: CardProps) => {
         <h3 className="product-name">{product.name}</h3>
         <p className="product-price">{product.price}</p>
       </div>
+      {/* <button className="card-button">+ Add to Cart</button> */}
+      <div className="quantity-controls">
+        <button className="quantity-button">-</button>
+        <span className="quantity">1 in Cart</span>
+        <button className="quantity-button">+</button>
+      </div>
+      <button className="remove-button">Remove</button>
     </div>
   );
 };
--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -6,7 +6,7 @@ import "../styles/components/store.css";
 const Store = () => {
   return (
     <div className="store-container">
-      <h1 className="store-title">Our Products</h1>
+      <h1 className="store-title">Store</h1>
       <div className="store-grid">
         {products.map((product) => (
           <Card key={product.id} product={product} />
