ceff6874f931290af8aaaabb58c41e34c5d1b778
Praneeth860 <mpraneeth474@gmail.com>
2024-12-11 22:25:49 +0530

    added responsiveness for nav and store

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -5,36 +5,38 @@ import CartIcon from "../assets/icons/CartIcon";
 const Navbar = () => {
   return (
     <nav className="navbar">
-      <ul className="navbar-list">
-        <li className="navbar-item">
-          <NavLink
-            to="/"
-            className={({ isActive }) => (isActive ? "active-link" : "")}
-          >
-            Home
-          </NavLink>
-        </li>
-        <li className="navbar-item">
-          <NavLink
-            to="/store"
-            className={({ isActive }) => (isActive ? "active-link" : "")}
-          >
-            Store
-          </NavLink>
-        </li>
-        <li className="navbar-item">
-          <NavLink
-            to="/about"
-            className={({ isActive }) => (isActive ? "active-link" : "")}
-          >
-            About
-          </NavLink>
-        </li>
-      </ul>
-      <button className="cart-button">
-        <CartIcon />
-        <span className="cart-badge">7</span>
-      </button>
+      <div className="nav_container">
+        <ul className="navbar-list">
+          <li className="navbar-item">
+            <NavLink
+              to="/"
+              className={({ isActive }) => (isActive ? "active-link" : "")}
+            >
+              Home
+            </NavLink>
+          </li>
+          <li className="navbar-item">
+            <NavLink
+              to="/store"
+              className={({ isActive }) => (isActive ? "active-link" : "")}
+            >
+              Store
+            </NavLink>
+          </li>
+          <li className="navbar-item">
+            <NavLink
+              to="/about"
+              className={({ isActive }) => (isActive ? "active-link" : "")}
+            >
+              About
+            </NavLink>
+          </li>
+        </ul>
+        <button className="cart-button">
+          <CartIcon />
+          <span className="cart-badge">7</span>
+        </button>
+      </div>
     </nav>
   );
 };
