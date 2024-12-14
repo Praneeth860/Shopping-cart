73f36d2d0f9fd844f75278edf822cda7dc41300c
Praneeth860 <mpraneeth474@gmail.com>
2024-12-14 22:12:19 +0530

    moved shopping cart to navbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,10 @@
 import Navbar from "./components/NavBar";
 import Routes from "./components/Routes";
-import ShoppingCart from "./components/ShoppingCart";
 const App = () => {
   return (
     <>
       <Navbar />
       <Routes />
-      <ShoppingCart />
     </>
   );
 };
--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,43 +1,47 @@
 import { NavLink } from "react-router-dom";
 import "../styles/components/navbar.css";
 import CartIcon from "../assets/icons/CartIcon";
-
+import ShoppingCart from "./ShoppingCart";
+import { useState } from "react";
 const Navbar = () => {
   return (
-    <nav className="navbar">
-      <div className="nav_container">
-        <ul className="navbar-list">
-          <li className="navbar-item">
-            <NavLink
-              to="/"
-              className={({ isActive }) => (isActive ? "active-link" : "")}
-            >
-              Home
-            </NavLink>
-          </li>
-          <li className="navbar-item">
-            <NavLink
-              to="/store"
-              className={({ isActive }) => (isActive ? "active-link" : "")}
-            >
-              Store
-            </NavLink>
-          </li>
-          <li className="navbar-item">
-            <NavLink
-              to="/about"
-              className={({ isActive }) => (isActive ? "active-link" : "")}
-            >
-              About
-            </NavLink>
-          </li>
-        </ul>
-        <button className="cart-button">
-          <CartIcon />
-          <span className="cart-badge">7</span>
-        </button>
-      </div>
-    </nav>
+    <>
+      <nav className="navbar">
+        <div className="nav_container">
+          <ul className="navbar-list">
+            <li className="navbar-item">
+              <NavLink
+                to="/"
+                className={({ isActive }) => (isActive ? "active-link" : "")}
+              >
+                Home
+              </NavLink>
+            </li>
+            <li className="navbar-item">
+              <NavLink
+                to="/store"
+                className={({ isActive }) => (isActive ? "active-link" : "")}
+              >
+                Store
+              </NavLink>
+            </li>
+            <li className="navbar-item">
+              <NavLink
+                to="/about"
+                className={({ isActive }) => (isActive ? "active-link" : "")}
+              >
+                About
+              </NavLink>
+            </li>
+          </ul>
+          <button className="cart-button">
+            <CartIcon />
+            <span className="cart-badge">7</span>
+          </button>
+        </div>
+      </nav>
+      {/* <ShoppingCart /> */}
+    </>
   );
 };
 
